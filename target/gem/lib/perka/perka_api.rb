ff0cdd88ad3d1f680248c942ba5919b0ffbcaaba
Joe Stelmach <joe@getperka.com>
2012-07-11 12:11:06 -0400

    new flatpack-rb session management

--- target/gem/lib/perka/perka_api.rb
+++ target/gem/lib/perka/perka_api.rb
@@ -7,6 +7,22 @@ module Perka
     
     attr_accessor :access_token, :access_expiration, :refresh_token, :user_uuid
     
+    # our session is considered active if we've never received an access
+    # token, or our token expiration as after now
+    def session_active?
+      !@access_token or @access_expiration > Time.now
+    end
+      
+    def refresh_session
+      oauth_refresh_token
+    end
+      
+    # Concrete implementations that require authentication should implement 
+    # this method if authenticated requests require custom request headers
+    def auth_headers
+      @access_token ? {'Authorization' => "Bearer #{@access_token}"} : {}
+    end
+    
     # Grants an integrator access token
     def oauth_integrator_login(integrator_id, integrator_secret)
       @integrator_id = integrator_id
@@ -29,6 +45,7 @@ module Perka
         :server_base => @server_base,
         :flatpack => @flatpack,
         :access_token => @access_token,
+        :access_expiration => @access_expiration,
         :verbose => @verbose,
       })
       execute_token_request(new_api, payload)
@@ -50,7 +67,6 @@ module Perka
     def execute_token_request(api, payload)
       json = TokenRequest.new(api, payload).execute
       status = json['status_code']
-      
       expires = json['expires_in']
       api.access_expiration = Time.now + expires.to_i if expires
       api.access_token = json['access_token']
